6e6a316fed1388865f4b96bd140eca443cba8e67
Amlan S. Behera <beheraamlan468@gmail.com>
2026-06-20 17:33:36 +0530

    Initial Commit

---
+++ SecureVault/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SecureVault
+//
+//  Created by Amlan Behera on 20/06/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SecureVault/SecureVaultApp.swift
@@ -0,0 +1,17 @@
+//
+//  SecureVaultApp.swift
+//  SecureVault
+//
+//  Created by Amlan Behera on 20/06/26.
+//
+
+import SwiftUI
+
+@main
+struct SecureVaultApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
